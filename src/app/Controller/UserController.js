50742257f46a2da31040da43a98b912b32f2ffd6
Từ Quân <callmequantu@gmail.com>
2023-12-26 17:42:45 +0700

    add find user by id

--- src/app/Controller/UserController.js
+++ src/app/Controller/UserController.js
@@ -8,5 +8,13 @@ const UserController = {
             return res.status(500).json({ message: err.message });
         }
     },
+    getUserById: async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            return res.status(200).json(user);
+        } catch (err) {
+            return res.status(500).json({ message: err.message });
+        }
+    },
 };
 module.exports = UserController;
--- src/route/user.js
+++ src/route/user.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../app/Controller/UserController');
 router.get('/all', userController.getAllUser);
+router.get('/:id', userController.getUserById);
 module.exports = router;
